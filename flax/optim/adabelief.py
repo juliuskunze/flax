2bfee4e46bcec878081cf62b60429e73923dbe33
Julius Kunze <juliuskunze@gmail.com>
2020-12-28 14:04:23 +0100

    Fix AdaBelief

--- flax/optim/adabelief.py
+++ flax/optim/adabelief.py
@@ -44,7 +44,7 @@ class AdaBelief(OptimizerDef):
                beta2=0.999,
                eps=1e-14,
                weight_decay=0.0):
-    """Constructor for the Adam optimizer.
+    """Constructor for the AdaBelief optimizer.
 
     Args:
       learning_rate: the step size used to update the parameters.
@@ -70,14 +70,14 @@ class AdaBelief(OptimizerDef):
     beta2 = hyper_params.beta2
     weight_decay = hyper_params.weight_decay
     grad_ema = beta1 * state.grad_ema + (1. - beta1) * grad
-    grad_sq_ema = beta2 * state.grad_sq_ema + (1. - beta2) * lax.square(grad - grad_ema)
+    grad_sq_ema = beta2 * state.grad_sq_ema + (1. - beta2) * lax.square(grad - grad_ema) + hyper_params.eps
 
     # bias correction
     t = step + 1.
     grad_ema_corr = grad_ema / (1 - beta1 ** t)
     grad_sq_ema_corr = (grad_sq_ema + hyper_params.eps) / (1 - beta2 ** t)
 
-    denom = jnp.sqrt(grad_sq_ema_corr) + hyper_params.eps
+    denom = jnp.sqrt(grad_sq_ema_corr)
     new_param = param - hyper_params.learning_rate * grad_ema_corr / denom
     new_param -= hyper_params.learning_rate * weight_decay * param
     new_state = _AdaBeliefParamState(grad_ema, grad_sq_ema)
